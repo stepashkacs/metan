268a1d39da9c58df5968842c6041d7d6ff3e2cbf
stepashka <stepashka@DESKTOP-GUJBD50>
2026-01-27 20:32:28 +0200

    Добавьте файлы проекта.

---
+++ metan/metan.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+//int main()
+//{
+//	int age1 = { 22 + 5 };
+//	int age2 = (22 + 5);
+//	int age3 = 22 + 5;
+//	std::cout << "age1 = " << age1 << '\n';
+//	std::cout << "age2 = " << age2 << '\n';
+//	std::cout << "age3 = " << age3 << '\n';
+//}
+
+
+//int main()
+//{
+//	bool isAlive{ true };
+//	bool isDead{ false };
+//	std::cout << "isAlive: " << isAlive << '\n';
+//	std::cout << "isDead: " << isDead << '\n';
+//}
+
+
+int main()
+{
+	signed char num1{ -64 };
+	unsigned char num2{ 64 };
+	short num3{ -88 };
+	unsigned short num4{ 88 };
+	int num5{ -1024 };
+	unsigned int num6{ 1024 };
+	long num7{ -2048 };
+	unsigned long num8{ 2048 };
+	long long num9{ -4096 };
+	unsigned long long num10{ 4096 };
+
+	std::cout << "num1" << num1 << '\n';
+	std::cout << "num2" << num2 << '\n';
+	std::cout << "num3" << num3 << '\n';
+	std::cout << "num4" << num4 << '\n';
+	std::cout << "num5" << num5 << '\n';
+	std::cout << "num6" << num6 << '\n';
+	std::cout << "num7" << num7 << '\n';
+	std::cout << "num8" << num8 << '\n';
+	std::cout << "num9" << num9 << '\n';
+	std::cout << "num10" << num10 << '\n';
+}
