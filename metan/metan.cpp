3f138609fbca47a23d463c50241f9601053d85c8
stepashka <cociorvastefan@gmail.com>
2026-01-29 12:45:03 +0200

    типы данных

--- metan/metan.cpp
+++ metan/metan.cpp
@@ -20,27 +20,88 @@
 //}
 
 
+//int main()
+//{
+//	signed char num1{ -64 };
+//	unsigned char num2{ 64 };
+//	short num3{ -88 };
+//	unsigned short num4{ 88 };
+//	int num5{ -1024 };
+//	unsigned int num6{ 1024U }; // U - unsigned int
+//	long num7{ -2048L }; // L - long
+//	unsigned long num8{ 2048UL }; // UL - unsigned long
+//	long long num9{ -4096LL }; // LL - long long
+//	unsigned long long num10{ 4096ULL }; // ULL - unsigned long long
+//
+//	std::cout << "num1: " << num1 << '\n';
+//	std::cout << "num2: " << num2 << '\n';
+//	std::cout << "num3: " << num3 << '\n';
+//	std::cout << "num4: " << num4 << '\n';
+//	std::cout << "num5: " << num5 << '\n';
+//	std::cout << "num6: " << num6 << '\n';
+//	std::cout << "num7: " << num7 << '\n';
+//	std::cout << "num8: " << num8 << '\n';
+//	std::cout << "num9: " << num9 << '\n';
+//	std::cout << "num10: " << num10 << '\n';
+//}
+
+//int main()
+//{
+//	int num1(1'2345'67890);
+//	int num2{ 0x1A };
+//	int num3{ 0xFF };
+//	int num4{ 0xFFFFFF };
+//	int num5{ 034 };
+//	int num6{ 0377 };
+//	int num7{ 0b11010 };
+//	int num8{ 0b11111111 };
+//	unsigned int num9{ 0b11010U };
+//	long num10{ 0377L };
+//	unsigned long num11{ 0xFFFFFFULL };
+//	std::cout << "num1= " << num1 << std::endl;
+//	std::cout << "num2= " << num2 << std::endl;
+//	std::cout << "num3= " << num3 << std::endl;
+//	std::cout << "num4= " << num4 << std::endl;
+//	std::cout << "num5= " << num5 << std::endl;
+//	std::cout << "num6= " << num6 << std::endl;
+//	std::cout << "num7= " << num7 << std::endl;
+//	std::cout << "num8= " << num8 << std::endl;
+//	std::cout << "num9= " << num9 << std::endl;
+//	std::cout << "num10= " << num10 << std::endl;
+//	std::cout << "num11= " << num11 << std::endl;
+//}
+
+
+/*int main()
+{
+	double num1{ 10.45 };
+	double num2{ 2 };
+	double num3{ 2. };
+	float num4{ 10.56f };
+	long double num5{ 10.561 };
+	double num6{ 5E3 };
+	double num7{ 2.5e-3 };
+	long double number{ 2 };
+	std::cout << "sizeof(number) =" << sizeof(number) << std::endl;
+	std::cout << "num1 = " << num1 << std::endl;
+	std::cout << "num2 = " << num2 << std::endl;
+	std::cout << "num3 = " << num3 << std::endl;
+	std::cout << "num4 = " << num4 << std::endl;
+	std::cout << "num5 = " << num5 << std::endl;
+	std::cout << "num6 = " << num6 << std::endl;
+	std::cout << "num7 = " << num7 << std::endl;
+}*/
+
+//int main()
+//{
+//	long double number{ 2 };
+//	std::cout << "sizeof(number) =" << sizeof(number);
+//}
+
 int main()
 {
-	signed char num1{ -64 };
-	unsigned char num2{ 64 };
-	short num3{ -88 };
-	unsigned short num4{ 88 };
-	int num5{ -1024 };
-	unsigned int num6{ 1024 };
-	long num7{ -2048 };
-	unsigned long num8{ 2048 };
-	long long num9{ -4096 };
-	unsigned long long num10{ 4096 };
-
-	std::cout << "num1" << num1 << '\n';
-	std::cout << "num2" << num2 << '\n';
-	std::cout << "num3" << num3 << '\n';
-	std::cout << "num4" << num4 << '\n';
-	std::cout << "num5" << num5 << '\n';
-	std::cout << "num6" << num6 << '\n';
-	std::cout << "num7" << num7 << '\n';
-	std::cout << "num8" << num8 << '\n';
-	std::cout << "num9" << num9 << '\n';
-	std::cout << "num10" << num10 << '\n';
+	char a1{ 'A' };
+	char a2{ 65 };
+	std::cout << "a1 = " << a1 << std::endl;
+	std::cout << "a2 = " << a2 << std::endl;
 }
